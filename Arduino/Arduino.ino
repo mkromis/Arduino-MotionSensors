9becc36ee7fb43548737a7814fd9c11e887b9a2b
Mark Kromis <143529142+mkromis@users.noreply.github.com>
2014-03-09 17:23:39 -0400

    Renamed files to match upper folder, adjusted for ERC issues, added initial Arduino sketch.

---
+++ Arduino/Arduino.ino
@@ -0,0 +1,13 @@
+/* 
+Arduino code to handle automation of lights.
+We currently are set up to handle 2 motion sensors,
+to trigger 2 independant relays. Basic ciruit, no frills.
+*/
+
+void setup()
+{
+}
+
+void loop()
+{
+}
